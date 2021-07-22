295ba0c53749f0943e7a834d1fd50a3779dc1f58
chosen2cwp <chosen1cwp@gmail.com>
2021-07-22 18:23:42 +0800

    update

--- cpSpider/items.py
+++ cpSpider/items.py
@@ -9,7 +9,11 @@ import scrapy
 class CpspiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    bule_ball = scrapy.Field();
-    red_ball = scrapy.Field();
-    cycle_No = scrapy.Field();
+    bule_ball = scrapy.Field(); # 蓝球
+    red_ball = scrapy.Field(); # 红球
+    cycle_No = scrapy.Field(); # 期号
+    winner_price_num = scrapy.Field(); # 中奖注数
+    open_date = scrapy.Field(); # 开奖日期
+    total_sale = scrapy.Field(); # 总销售额
+    jackpot = scrapy.Field(); # 奖池金额
     pass
